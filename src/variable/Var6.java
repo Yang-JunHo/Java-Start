38a7bd0c398810dbce0ec79334971574cf8a4d9e
Yang-JunHo <yjy3070@naver.com>
2024-12-09 16:21:47 +0900

    variable

---
+++ src/variable/Var6.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        // System.out.println(a);
+        // 변수 초기화는 무조건 해야함 -> 초기화하지 않으면 컴파일 오류 발생!
+
+    }
+}
---
+++ src/variable/Var8.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        // 정수
+        byte b = 127; // -128 ~ 127
+        short s = 32767; // -32,768 ~ 32,767
+        int i = 2147483647; // -2,147,483,648 ~ 2,147,483,647 (약 20억)
+        long l = 9223372L; // 엄청 많은 수까지 작성 가능
+
+        // 실수
+        float f = 10.0f;
+        double d = 10.0;
+    }
+}
